62cb5615f704bd4c47d5cde5405ff61e0a6e74a8
irvanln <irvanakia@gmail.com>
2023-07-27 19:31:03 +0700

    INSERT UPDATE DELETE Database

--- Jdbc1/src/app/MainClass.java
+++ Jdbc1/src/app/MainClass.java
@@ -9,12 +9,10 @@ package app;
  * @author irvin
  */
 
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.util.logging.Logger;
 import java.util.logging.Level;
-import java.sql.ResultSet;
+import java.sql.*;
 public class MainClass {
     
     private static final String uname ="root";
@@ -25,15 +23,29 @@ public class MainClass {
     public static void main (String[] args){
         try {
             Connection koneksi = DriverManager.getConnection(url,uname,passwd);
-            Statement st =  koneksi.createStatement();
-            System.out.println("Database ditemukan");
-            ResultSet hasil = st.executeQuery("SELECT * FROM mahasiswa");
-            
-            while(hasil.next()){
-                System.out.println("Nama : " +hasil.getString("nama"));
-                System.out.println("NIM : "+hasil.getString(2));
-                
-            }
+//            Statement st =  koneksi.createStatement();
+//            System.out.println("Database ditemukan");
+//            ResultSet hasil = st.executeQuery("SELECT * FROM mahasiswa");
+//            
+//            while(hasil.next()){
+//                System.out.println("Nama : " +hasil.getString("nama"));
+//                System.out.println("NIM : "+hasil.getString(2));
+//                
+//            }
+
+ //   PreparedStatement ps = koneksi.prepareStatement("INSERT into mahasiswa (nim,nama)values(?,?)");
+//  PreparedStatement ps = koneksi.prepareStatement("UPDATE mahasiswa set nama =? where nim =?");   
+// ps.setString(2,"123400");
+//    ps.setString(1,"irvan 3 update");
+//    ps.executeUpdate();
+//        
+
+ PreparedStatement ps = koneksi.prepareStatement("delete from mahasiswa where nim =?");   
+ ps.setString(1,"123400");
+    
+    ps.executeUpdate();
+        
+ps.close();
             koneksi.close();
             
         } catch (Exception e) {
